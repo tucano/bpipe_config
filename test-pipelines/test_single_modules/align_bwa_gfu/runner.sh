15b5af85eb02c7c581b1562c6fee0545f88a5d66
tucano <davide.rambaldi@gmail.com>
2013-10-27 20:36:55 +0100

    Multi input tests

--- test-pipelines/test_single_modules/align_bwa_gfu/runner.sh
+++ test-pipelines/test_single_modules/align_bwa_gfu/runner.sh
@@ -45,6 +45,12 @@ if [[ $RES != 4 ]]; then
     echo "Error for For multiple input fastq I expect 4 output files"
     exit 1
 fi
+bpipe query > test.graph
+RESULT=`diff expected_multi.graph test.graph`
+if [[ $RESULT > 0 ]]; then
+    echo "Error for multi input, dependency graph"
+    exit 1
+fi
 ./cleaner.sh
 
 # MULTIPLE FASTQ.GZ
@@ -52,7 +58,13 @@ bpipe run test.groovy ../../data/*.fastq.gz
 # I expecd 4 files
 RES=`ls *.sai | wc | awk {'print $1'}`
 if [[ $RES != 4 ]]; then
-    echo "Error for For multiple input fastq I expect 4 output files"
+    echo "Error for For multiple input fastq.gz I expect 4 output files"
+    exit 1
+fi
+bpipe query > test.graph
+RESULT=`diff expected_multi_gz.graph test.graph`
+if [[ $RESULT > 0 ]]; then
+    echo "Error for multi input.gz, dependency graph"
     exit 1
 fi
 ./cleaner.sh
