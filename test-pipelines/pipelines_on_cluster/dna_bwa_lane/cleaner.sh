ce986aefefb904392f89787a83e38f0ece2a8758
tucano <davide.rambaldi@gmail.com>
2013-11-18 11:53:03 +0100

    Remove test pipelines on cluster dirs

--- test-pipelines/pipelines_on_cluster/dna_bwa_lane/cleaner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 0. clean
-bpipe cleanup -y
-
-# 1. clean output and logs
-rm -rf .bpipe read* *.sai *.bam *merge* *dedup* commandlog.txt run.log run.err *.junc doc bpipe.config *.groovy gfu_environment.sh
-
