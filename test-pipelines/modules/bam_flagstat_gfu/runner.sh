1265fb63d9940a106874ba093d1ba30ff0c355a7
tucano <davide.rambaldi@gmail.com>
2014-01-09 12:50:20 +0100

    bam flagstat test

--- test-pipelines/modules/bam_flagstat_gfu/runner.sh
+++ test-pipelines/modules/bam_flagstat_gfu/runner.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-SCRIPT_NAME="test_bam_flagstat_gfu_module"
-INPUTONE="../../data/testinput_one.bam"
-OUTPUTONE="testinput_one.log"
-
 ./cleaner.sh
 
-bpipe run test.groovy $INPUTONE
-if [[ ! -f $OUTPUTONE ]]; then
-    echo "Error for $OUTPUTONE"
-    exit 1
-fi
-bpipe query > test.graph
-RESULT=`diff expected.graph test.graph`
-if [[ $RESULT > 0 ]]; then
-    echo "Error for $OUTPUTONE , dependency graph"
+bpipe run test.groovy testinput_one.bam > test.out
+grep 'Pipeline failed!' test.out 1>/dev/null 2>&1
+if [[ $? == 0 ]]; then
+    echo "FAIL"
     exit 1
 fi
 ./cleaner.sh
