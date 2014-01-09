b5b6e0563c95046a68c155f970e476c01e4ea3f4
tucano <davide.rambaldi@gmail.com>
2014-01-09 13:08:41 +0100

    fastq_lnae test

--- test-pipelines/modules/fastqc_lane_gfu/runner.sh
+++ test-pipelines/modules/fastqc_lane_gfu/runner.sh
@@ -2,18 +2,18 @@
 
 ./cleaner.sh
 
-bpipe run test.groovy ../../data/*.fastq.gz
-# I expect 2 files
-RES=`ls *.zip | wc | awk {'print $1'}`
-if [[ $RES != 2 ]]; then
+bpipe run test.groovy *.fastq.gz > test.out
+grep 'Pipeline failed!' test.out 1>/dev/null 2>&1
+if [[ $? == 0 ]]; then
+    echo "FAIL"
     exit 1
 fi
 ./cleaner.sh
 
-bpipe run test_single.groovy ../../data/*.fastq.gz
-# I expect 1 files
-RES=`ls *.zip | wc | awk {'print $1'}`
-if [[ $RES != 1 ]]; then
+bpipe run test_single.groovy *.fastq.gz > test.out
+grep 'Pipeline failed!' test.out 1>/dev/null 2>&1
+if [[ $? == 0 ]]; then
+    echo "FAIL"
     exit 1
 fi
 ./cleaner.sh
