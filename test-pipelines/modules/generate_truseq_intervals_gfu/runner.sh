abdb84336c235cd61684d53fad1e6da777c16ae2
tucano <davide.rambaldi@gmail.com>
2014-01-09 13:13:56 +0100

    generate_truseq_intervals_gfu test

--- test-pipelines/modules/generate_truseq_intervals_gfu/runner.sh
+++ test-pipelines/modules/generate_truseq_intervals_gfu/runner.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-SCRIPT_NAME="test_generate_truseq_intervals_module"
-
 ./cleaner.sh
 
-bpipe run test.groovy ../../data/*.bam
-# I expect 24 files
-RES=`ls *.intervals | wc | awk {'print $1'}`
-if [[ $RES != 24 ]]; then
+bpipe run test.groovy *.bam > test.out
+grep 'Pipeline failed!' test.out 1>/dev/null 2>&1
+if [[ $? == 0 ]]; then
+    echo "FAIL"
     exit 1
 fi
-
 ./cleaner.sh
 
 echo "SUCCESS"
