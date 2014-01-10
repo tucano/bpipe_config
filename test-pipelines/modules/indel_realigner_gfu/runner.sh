a86006891d3f6baaeb7e9f96e0a2aa077d2d2f65
tucano <davide.rambaldi@gmail.com>
2014-01-10 13:38:05 +0100

    indel_realigner_gfu test

--- test-pipelines/modules/indel_realigner_gfu/runner.sh
+++ test-pipelines/modules/indel_realigner_gfu/runner.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-SCRIPT_NAME="test_indel_realigner_module"
-INPUTBAM="../../data/testinput_one.bam"
-INPUTINTERVALS="../../data/testinput_one.intervals"
-OUTPUTONE="testinput_one.indel_realigned.bam"
-
 ./cleaner.sh
 
-bpipe run test.groovy $INPUTBAM $INPUTINTERVALS
-if [[ ! -f $OUTPUTONE ]]; then
-    echo "Error for $OUTPUTONE"
-    exit 1
-fi
-bpipe query > test.graph
-RESULT=`diff expected.graph test.graph`
-if [[ $RESULT > 0 ]]; then
-    echo "Error for $OUTPUTONE , dependency graph"
+bpipe run test.groovy testinput_one.bam testinput_one.intervals > test.out
+grep 'Pipeline failed!' test.out 1>/dev/null 2>&1
+if [[ $? == 0 ]]; then
+    echo "FAIL"
     exit 1
 fi
 ./cleaner.sh
