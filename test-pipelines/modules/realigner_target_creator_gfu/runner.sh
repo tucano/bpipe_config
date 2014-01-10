a0f7cd11d418bf407c1b106f1c2921100a59c44a
tucano <davide.rambaldi@gmail.com>
2014-01-10 15:42:44 +0100

    tests

--- test-pipelines/modules/realigner_target_creator_gfu/runner.sh
+++ test-pipelines/modules/realigner_target_creator_gfu/runner.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-SCRIPT_NAME="test_realign_target_creator_module"
-INPUTBAM="../../data/testinput_one.bam"
-OUTPUTONE="testinput_one.intervals"
-
 ./cleaner.sh
 
-bpipe run test.groovy $INPUTBAM
-if [[ ! -f $OUTPUTONE ]]; then
-    echo "Error for $OUTPUTONE"
-    exit 1
-fi
-bpipe query > test.graph
-RESULT=`diff expected.graph test.graph`
-if [[ $RESULT > 0 ]]; then
-    echo "Error for $OUTPUTONE , dependency graph"
+bpipe run test.groovy testinput_one.bam
+grep 'Pipeline failed!' test.out 1>/dev/null 2>&1
+if [[ $? == 0 ]]; then
+    echo "FAIL"
     exit 1
 fi
 ./cleaner.sh
