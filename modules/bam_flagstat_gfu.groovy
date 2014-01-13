6ea2d8d21c322bf3028c50f99c84178486c7433e
tucano <davide.rambaldi@gmail.com>
2014-01-13 12:40:36 +0100

    flagstat

--- modules/bam_flagstat_gfu.groovy
+++ modules/bam_flagstat_gfu.groovy
@@ -4,24 +4,39 @@ SAMTOOLS="/usr/local/cluster/bin/samtools"
 @preserve
 bam_flagstat_gfu =
 {
-    var test : false
+    var pretend : false
 
-    doc title: "Flagstat on BAM file",
-        desc: "Launch $SAMTOOLS flagstat on the final (merged) bam file, produce a log file $output",
-        constraints: "Generate a LOG file and forward input to next stage",
+    doc title: "samtools flagstat on BAM file",
+        desc: """
+            Launch $SAMTOOLS flagstat on ${input.bam}, produce a log file: $output.
+            Main options with default value:
+                pretend    : $pretend
+
+            Generate a .log file and forward ${input.bam} to next stage.
+        """,
+        constraints: "...",
         author: "davide.rambaldi@gmail.com"
 
-    transform("log") {
+
+    transform("log") 
+    {
         def command = """
-            echo -e "[bam_flagstat_gfu]: flagstat with input $input.bam and output file $output" >&2;
             $SAMTOOLS flagstat $input.bam > $output;
         """
-        if (test) {
-            println "INPUT $input, OUTPUT: $output"
-            println "COMMAND: $command"
-            command = "touch $output"
+        if (pretend) 
+        {
+            println """
+                INPUT:  $input
+                OUTPUT: $output
+                COMMAND:
+                    $command
+            """
+            command = """
+                echo "INPUT: $input" > $output
+            """
         }
         exec command
     }
+
     forward input.bam
 }
--- test-pipelines/modules/bam_flagstat_gfu/test.groovy
+++ test-pipelines/modules/bam_flagstat_gfu/test.groovy
@@ -1,5 +1,14 @@
 load "../../../modules/bam_flagstat_gfu.groovy"
 
+REFERENCE_GENOME="/test/reference/pippo.fa"
+EXPERIMENT_NAME="test"
+PLATFORM="test"
+FCID="1"
+EXPERIMENT_NAME="test1"
+SAMPLEID="S1"
+CENTER="GFU"
+PROJECTNAME="TEST_1_TEST"
+
 Bpipe.run {
-    "*" * [bam_flagstat_gfu.using(test:true)]
+    "*" * [bam_flagstat_gfu.using(pretend:true)]
 }
