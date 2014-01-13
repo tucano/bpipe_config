fb63f78a234c135cc05aa6ec2c74559613e8ec8c
tucano <davide.rambaldi@gmail.com>
2014-01-13 14:20:43 +0100

    fastqc sample

--- modules/fastqc_sample_gfu.groovy
+++ modules/fastqc_sample_gfu.groovy
@@ -4,18 +4,28 @@ FASTQC="/lustre1/tools/bin/fastqc"
 @intermediate
 fastqc_sample_gfu =
 {
-    var paired : true
+    // stage vars
+    var paired  : true
+    var pretend : false
 
     doc title: "Fastqc on fastq.gz files in a sample directory",
         desc: """
-            This module is for Illumina project structure,
-            use fastqc_lane_gfu for single sample
+            This module is for Illumina project structure, use fastqc_lane_gfu for single sample directory.
+            Take as input a directory named: SAMPLE_NAME_report ($input) 
+            and generate in it fastqc files from the corresponding sample.
+
+            Main options with value:
+            pretend          : $pretend
+            paired           : $paired
         """,
-        constraints: "...",
+        constraints: """
+            Need a strict directory structure: Sample_1, Sample_1_report, ... , Sample_N, Sample_N_report
+        """,
         author: "davide.rambaldi@gmail.com"
 
-
+    // GET INPUT DIR (report dir) AND SET OUTPUT DIR()
     output.dir = input
+    // GET DATA DIR and locate input files
     def data_dir = input.replaceAll("_report","")
     def input_files = []
     def dir = new File(data_dir)
@@ -25,6 +35,7 @@ fastqc_sample_gfu =
         }
     }
 
+    // DEFINE OUTPUT PREFIX
     def output_prefix
     if (paired) {
         if (input_files.size <= 2) {
@@ -40,17 +51,51 @@ fastqc_sample_gfu =
         }
     }
 
-    println "INPUT DATA IN DIR: $data_dir INPUT REPORT DIR: $input INPUT FILES: $input_files OUTPUT PREFIX: ${output_prefix}"
-    if (paired) {
-        produce("${output_prefix[0]}_fastqc.zip","${output_prefix[1]}_fastqc.zip") {
-            println "OUTPUTS $outputs"
+    if (paired) 
+    {
+        produce("${output_prefix[0]}_fastqc.zip","${output_prefix[1]}_fastqc.zip") 
+        {
+            def command = "$FASTQC -f fastq --noextract --casava --nogroup -t 4 -o $input ${data_dir}/*.fastq.gz;"
+
+            if (pretend)
+            {
+                println """
+                    DATA_DIR: $data_dirs
+                    REPORT_DIR: $input
+                    INPUT FILES: $input_files
+                    OUTPUT PREFIX: ${output_prefix}
+                """
+                command = """
+                    echo "INPUTS: $inputs" > $output1
+                    echo "INPUTS: $inputs" > $output2
+                """
+            }
+
+            exec command, "fastqc"
         }
-    } else {
-        produce("${output_prefix[0]}_fastqc.zip") {
-            println "OUTPUT $output"
+    }
+    else
+    {
+        produce("${output_prefix[0]}_fastqc.zip") 
+        {
+            def command = "$FASTQC -f fastq --noextract --casava --nogroup -t 4 -o $input ${data_dir}/*.fastq.gz;"
+            
+            if (pretend)
+            {
+                println """
+                    DATA_DIR: $data_dirs
+                    REPORT_DIR: $input
+                    INPUT FILES: $input_files
+                    OUTPUT PREFIX: ${output_prefix}
+                """
+                command = """
+                    echo "INPUTS: $inputs" > $output
+                """
+            }
+
+            exec command, "fastqc"
         }
     }
 
-    exec "$FASTQC -f fastq --noextract --casava --nogroup -t 4 -o $input ${data_dir}/*.fastq.gz;"
     forward input
 }
---
+++ test-pipelines/modules/fastqc_sample_gfu/runner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ../../testsupport.sh
+
+./cleaner.sh
+
+OUTPUTS=(Sample_test_1_report/Sample_test_1_testinput_R1_fastqc.zip Sample_test_2_report/Sample_test_2_testinput_R1_fastqc.zip)
+run test_single.groovy Sample_test_*_report
+checkTestOut
+exists $OUTPUTS
+./cleaner.sh
+
+OUTPUTS=(Sample_test_1_report/Sample_test_1_testinput_R1_fastqc.zip Sample_test_1_report/Sample_test_1_testinput_R2_fastqc.zip Sample_test_2_report/Sample_test_2_testinput_R1_fastqc.zip Sample_test_2_report/Sample_test_2_testinput_R2_fastqc.zip)
+run test_paired.groovy Sample_test_*_report
+checkTestOut
+exists $OUTPUTS
+./cleaner.sh
+
+success
