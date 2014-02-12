e196a69e51d52b3e4a8c3e00f60800d6dea38aa1
drambaldi <davide.rambaldi@gmail.com>
2014-02-12 15:33:32 +0100

    fixing variants annotation test

--- test-pipelines/pipelines/variants_annotation/runner.sh
+++ test-pipelines/pipelines/variants_annotation/runner.sh
@@ -5,9 +5,9 @@ source ../../testsupport.sh
 
 OUTPUTS=(Tier2.xls)
 
-config human_variants_annotation
-sed 's/\/lustre1\/workspace\/Stupka\/HealthyExomes\//healty_exomes/' human_variants_annotation.groovy | diff -p human_variants_annotation.groovy /dev/stdin | patch 1>/dev/null
-runPipeLine human_variants_annotation.groovy input.vcf
+config variants_annotation
+sed 's/\/lustre1\/workspace\/Stupka\/HealthyExomes\//healty_exomes/' variants_annotation.groovy | diff -p variants_annotation.groovy /dev/stdin | patch 1>/dev/null
+runPipeLine variants_annotation.groovy input.vcf
 checkTestOut
 exists $OUTPUTS
 ./cleaner.sh
