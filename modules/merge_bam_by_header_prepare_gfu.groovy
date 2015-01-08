ee5f032dae65d6f5a195e95871d959be9cb91df6
Davide <davide.rambaldi@gmail.com>
2015-01-08 15:08:45 +0100

    Module prepare bam headers

---
+++ modules/merge_bam_by_header_prepare_gfu.groovy
@@ -0,0 +1,38 @@
+// MODULE MERGE BAM PREPARE(rev1)
+import static groovy.json.JsonOutput.*
+
+@preserve
+merge_bam_by_header_prepare_gfu =
+{
+    doc title: "Prepare Merge bam files with $PICMERGE using bam headers SM",
+        desc: "",
+        constraints: "",
+        author: "davide.rambaldi@gmail.com"
+
+    produce("merge_rule.json")
+    {
+        exec """
+            for i in ${inputs.bam};
+            do SAMPLE=`$SAMTOOLS view -H \$i | grep SM | sed -e 's/.*SM://' | sed -e 's/CN:.*//'`;
+            echo -e "\$i\t\$SAMPLE";
+            done > merge_rule.txt
+        """
+
+        def bams = [:]
+        new File("merge_rule.txt").eachLine { line ->
+            data = line.split("\t")
+            if (bams[data[1]])
+            {
+                bams[data[1]].push data[0]
+            }
+            else
+            {
+                bams[data[1]] = [data[0]]
+            }
+        }
+
+        def w = new File(output1).newWriter()
+        w << prettyPrint(toJson(bams))
+        w.close()
+    }
+}
